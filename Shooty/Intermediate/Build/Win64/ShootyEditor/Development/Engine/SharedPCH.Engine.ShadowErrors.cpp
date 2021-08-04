8d8600b6d5ed04ffca6bef63fbd73140bfee5f00
DivadTenni <davidctenni@gmail.com>
2021-08-04 12:59:18 +1000

    Inital Project Commit

---
+++ Shooty/Intermediate/Build/Win64/ShootyEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30038
+#include "C:/GameDev/Shooty/Shooty/Intermediate/Build/Win64/ShootyEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Shooty/Intermediate/Build/Win64/UnrealEditor/Inc/Shooty/ShootyGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SHOOTY_ShootyGameModeBase_generated_h
+#error "ShootyGameModeBase.generated.h already included, missing '#pragma once' in ShootyGameModeBase.h"
+#endif
+#define SHOOTY_ShootyGameModeBase_generated_h
+
+#define Shooty_Source_Shooty_ShootyGameModeBase_h_15_SPARSE_DATA
+#define Shooty_Source_Shooty_ShootyGameModeBase_h_15_RPC_WRAPPERS
+#define Shooty_Source_Shooty_ShootyGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define Shooty_Source_Shooty_ShootyGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAShootyGameModeBase(); \
+	friend struct Z_Construct_UClass_AShootyGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AShootyGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Shooty"), NO_API) \
+	DECLARE_SERIALIZER(AShootyGameModeBase)
+
+
+#define Shooty_Source_Shooty_ShootyGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAShootyGameModeBase(); \
+	friend struct Z_Construct_UClass_AShootyGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AShootyGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Shooty"), NO_API) \
+	DECLARE_SERIALIZER(AShootyGameModeBase)
+
+
+#define Shooty_Source_Shooty_ShootyGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AShootyGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AShootyGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AShootyGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AShootyGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AShootyGameModeBase(AShootyGameModeBase&&); \
+	NO_API AShootyGameModeBase(const AShootyGameModeBase&); \
+public:
+
+
+#define Shooty_Source_Shooty_ShootyGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AShootyGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AShootyGameModeBase(AShootyGameModeBase&&); \
+	NO_API AShootyGameModeBase(const AShootyGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AShootyGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AShootyGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AShootyGameModeBase)
+
+
+#define Shooty_Source_Shooty_ShootyGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define Shooty_Source_Shooty_ShootyGameModeBase_h_12_PROLOG
+#define Shooty_Source_Shooty_ShootyGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Shooty_Source_Shooty_ShootyGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Shooty_Source_Shooty_ShootyGameModeBase_h_15_SPARSE_DATA \
+	Shooty_Source_Shooty_ShootyGameModeBase_h_15_RPC_WRAPPERS \
+	Shooty_Source_Shooty_ShootyGameModeBase_h_15_INCLASS \
+	Shooty_Source_Shooty_ShootyGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Shooty_Source_Shooty_ShootyGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Shooty_Source_Shooty_ShootyGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Shooty_Source_Shooty_ShootyGameModeBase_h_15_SPARSE_DATA \
+	Shooty_Source_Shooty_ShootyGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	Shooty_Source_Shooty_ShootyGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	Shooty_Source_Shooty_ShootyGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> SHOOTY_API UClass* StaticClass<class AShootyGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Shooty_Source_Shooty_ShootyGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
